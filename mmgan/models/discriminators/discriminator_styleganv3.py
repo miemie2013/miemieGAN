53a19c43b8b6093aeb1418de4f8e322a02a5e661
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-03-25 12:01:19 +0800

    f

--- mmgan/models/discriminators/discriminator_styleganv3.py
+++ mmgan/models/discriminators/discriminator_styleganv3.py
@@ -85,28 +85,6 @@ class DiscriminatorBlock(nn.Module):
         assert x.dtype == dtype
         return x, img
 
-class MinibatchStdLayer(nn.Module):
-    def __init__(self, group_size, num_channels=1):
-        super().__init__()
-        self.group_size = group_size
-        self.num_channels = num_channels
-
-    def forward(self, x):
-        N, C, H, W = x.shape
-        G = min(self.group_size, N) if self.group_size is not None else N
-        F = self.num_channels
-        c = C // F
-
-        y = x.reshape((G, -1, F, c, H, W))    # [GnFcHW] Split minibatch N into n groups of size G, and channels C into F groups of size c.
-        y = y - y.mean(0)                   # [GnFcHW] Subtract mean over group.
-        y = y.square().mean(0)              # [nFcHW]  Calc variance over group.
-        y = (y + 1e-8).sqrt()               # [nFcHW]  Calc stddev over group.
-        y = y.mean([2, 3, 4])               # [nF]     Take average over channels and pixels.
-        y = y.reshape((-1, F, 1, 1))          # [nF11]   Add missing dimensions.
-        y = y.repeat([G, 1, H, W])            # [NFHW]   Replicate over group and pixels.
-        x = torch.cat([x, y], 1)            # [NCHW]   Append to input as new channels.
-        return x
-
 
 class StyleGANv3_DMappingNetwork(torch.nn.Module):
     def __init__(self,
